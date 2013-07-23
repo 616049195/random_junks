f32f31800a432136962de3d7e6eb954b6add3d9a
Hyunchel Kim <hyunchelkk@gmail.com>
2013-07-23 17:37:28 -0500

    GUI

---
+++ small_projects/FirstGUI.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class FirstGUI {
+	public static void main(String[] args) {
+		JFrame _frame = new JFrame();
+		
+		JButton _button = new JButton("Click here!");
+
+		_frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		_frame.getContentPane().add(_button);
+		_frame.setSize(300, 300);
+		_frame.setVisible(true);
+	}
+}
