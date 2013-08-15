801a5f6bd36fc24a996537306f5fc7f54df209e0
Hyunchel <hyunchelkk@gmail.com>
2013-08-15 15:53:19 -0500

    battleship complete

--- random_games/battleship.py
+++ random_games/battleship.py
@@ -1,4 +1,21 @@
+"""
+Welcome to Battleship. 
+This is a noobie version. 
+Only purpose was to learn Python.
+
+1 Player game.
+Grid size = 5x5
+4 turns.
+
+2014-08-15
+by ...
+"""
+
 from random import randint
+
+"""
+Preparing for the Game
+"""
 
 board = []
 
@@ -9,30 +26,34 @@ def print_board(board):
     for row in board:
         print " ".join(row)
 
-print "Let's play Battleship!"
-print_board(board)
-
 def random_row(board):
     return randint(0, len(board) - 1)
 
 def random_col(board):
     return randint(0, len(board[0]) - 1)
 
+
+"""
+Game begins
+"""
+
+print "Let's play Battleship!"
+print_board(board)
+
 ship_row = random_row(board)
 ship_col = random_col(board)
-print ship_row
-print ship_col
 
-# Everything from here on should go in your for loop!
-# Be sure to indent four spaces!
+# user gets 4 turns.
 for turn in range(4):
+    # take user inputs
     guess_row = int(raw_input("Guess Row:"))
     guess_col = int(raw_input("Guess Col:"))
     
+    # correct guess
     if guess_row == ship_row and guess_col == ship_col:
         print "Congratulations! You sunk my battleship!"
         break
-    else:
+    else: # incorrect guesses
         if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
             print "Oops, that's not even in the ocean."
         elif(board[guess_row][guess_col] == "X"):
@@ -40,8 +61,10 @@ for turn in range(4):
         else:
             print "You missed my battleship!"
             board[guess_row][guess_col] = "X"
-        # Print (turn + 1) here!
+        # prints the board, turn, and "Game Over" message no matter what.
         print_board(board)
         print turn+1
         if turn == 3:
             print "Game Over"
+
+# code ends.
