f00f872904b10fd8b46afe67bdf4c77378813439
Hyunchel Kim <hyunchelkk@gmail.com>
2013-09-10 21:51:26 -0500

    Exception successfully thwon

--- security_projects/project_1/BadInstructionObject.java
+++ security_projects/project_1/BadInstructionObject.java
@@ -1,4 +1,13 @@
-public final class BadInstructionObject {
+// import java.lang.Exception;
+
+public final class BadInstructionObject extends Exception {
+	final String message = "BadInstructionObject";
+
+	@Override
+	public String getMessage () {
+		return message;
+	}
+
 	public void printWelcome () {
 		System.out.println("Hello, welcome to BadInstructionObject final class!");
 	}
