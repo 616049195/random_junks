2172b03863c92e10a0cc40e58614ae0816c37421
Hyunchel Kim <hyunchelkk@gmail.com>
2013-09-10 21:50:37 -0500

    subjects objects initialized with names and values

--- security_projects/project_1/Objects.java
+++ security_projects/project_1/Objects.java
@@ -1,4 +1,14 @@
 public class Objects {
+	String name;
+	int level;
+	int current_value;
+
+	public Objects (String name, int level) {
+		this.name = name;
+		this.level = level;
+		this.current_value = 0;
+	}
+
 	public void printWelcome () {
 		System.out.println("Hello, welcome to Objects class!");
 	}
--- security_projects/project_1/Subjects.java
+++ security_projects/project_1/Subjects.java
@@ -1,4 +1,14 @@
 public class Subjects {
+	String name;
+	int level;
+	int temp;
+
+	public Subjects (String name, int level) {
+		this.name = name;
+		this.level = level;
+		this.temp = 0;
+	}
+
 	public void printWelcome () {
 		System.out.println("Hello, welcome to Subjects class!");
 
