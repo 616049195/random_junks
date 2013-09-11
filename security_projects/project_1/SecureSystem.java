8244400307a9ffcde8950b96c7b00e046d1cb501
Hyunchel Kim <hyunchelkk@gmail.com>
2013-09-10 20:35:04 -0500

    file_read

--- security_projects/project_1/SecureSystem.java
+++ security_projects/project_1/SecureSystem.java
@@ -1,3 +1,4 @@
+import java.io.*;
 public class SecureSystem {
 	public static void main(String[] args) {
 		// create subjects with security label
@@ -5,7 +6,26 @@ public class SecureSystem {
 		// create objects with security label
 
 		// read command line
+		System.out.println("Hello!");
 		
+		System.out.println(args[0]);			
+			// open file (name in args[0])
+
+
+	try {		
+	    FileReader fr = new FileReader(args[0]); 
+		BufferedReader br = new BufferedReader(fr); 
+		String s; 
+		while((s = br.readLine()) != null) { 
+		System.out.println(s); 
+		} 
+		fr.close(); 
+	}
+	catch(Exception e) {
+		System.out.println("Error!!! ");
+		e.printStackTrace();
+	}
+		System.out.println("Done!.");
 		// execute commands
 
 		// print states 
