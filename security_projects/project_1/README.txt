a85fc3490aac8a1056706c7124e18771e7dfa821
Hyunchel Kim <hyunchelkk@gmail.com>
2013-09-10 19:48:45 -0500

    init

---
+++ security_projects/project_1/README.txt
@@ -0,0 +1,3 @@
+Name: Hyunchel Kim
+Email: hyunchelkk@gmail.com / hyunchelkim@utexas.edu
+UTEID: hk6674
---
+++ security_projects/project_1/SecureSystem.java
@@ -0,0 +1,13 @@
+public class SecureSystem {
+	public static void main(String[] args) {
+		// create subjects with security label
+
+		// create objects with security label
+
+		// read command line
+		
+		// execute commands
+
+		// print states 
+	}
+}
