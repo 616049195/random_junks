f8004410c7231494a838a53bd4d05ffca2b089a3
Hyunchel <hyunchelkk@gmail.com>
2013-11-22 15:32:21 -0600

    updates

--- enrich/Enrich.java
+++ enrich/Enrich.java
@@ -33,12 +33,6 @@ class Data {
 		}
 	}
 
-	public static boolean deleteProfile (String name) {
-		File _info = new File("./data/" + name);
-		_info.delete();
-		return true;	
-	}
-
 	public static boolean editProfile (String name, double amount) {
 		// file name should be an unique user id at some point..
 		try {
@@ -58,6 +52,12 @@ class Data {
 			e.printStackTrace();
 			return false;
 		}
+	}
+
+	public static boolean deleteProfile (String name) {
+		File _info = new File("./data/" + name);
+		_info.delete();
+		return true;	
 	}
 
 	public static double readProfile (String name) {
@@ -133,8 +133,8 @@ class Account {
 		_name = name;
 		// have a random hash? to produce an unique id.
 
-		Data.createProfile(name);
-		Data.editProfile(name, amount);
+		assert (Data.createProfile(name));
+		assert (Data.editProfile(name, amount));
 	}
 
 	public String get_name () {
@@ -151,6 +151,12 @@ class Account {
 
 	public void withdraw (double amount) {
 		_balance -= amount;
+	}
+
+	public void write () {
+		// assert (_name.equals("Hyunchel Kim"));
+		// assert (_balance == 200.00);
+		assert (Data.editProfile(_name, _balance));
 	}
 }
 
@@ -172,25 +178,73 @@ public class Enrich {
 
 		double amount = Data.readProfile(name);
 		Account ac1 = new Account(name, amount);
+		System.out.println("Amount stored: " + ac1.get_balance());
 		System.out.println("Your account has been loaded.\n");
 		return ac1;
 	}
 	
+	public int show_menu () {
+		// show options - use enum..?
+		
+		// 1. See your balance
+		// 2. Deposit
+		// 3. Withdraw
+		// 4. Exit
+
+		System.out.println(	"Options\n"
+						+	"1) See your balance\n"
+						+	"2) Deposit\n"
+						+	"3) Withdraw\n"
+						+	"4) Exit\n");				
+
+		Console console = System.console();
+		String input = console.readLine("Choose your action: ");
+		return Integer.parseInt(input);
+	}
 
 	public static void main (String[] args) {
 
 		System.out.println("Enrich is about to make you rich.\n");
 
-		Enrich p1 = new Enrich();
+		Enrich rich = new Enrich();
 		Account ac1;
 		if (Data.isNew()) {
 			Data.makeDir();
-			ac1 = p1.new_user();
+			ac1 = rich.new_user();
 		}
 		else {
-			ac1 = p1.get_user();
+			ac1 = rich.get_user();
+			System.out.println(ac1.get_balance());
+			// assert (ac1.get_balance() == 200.00);
 		}
 
+		int action = 0;
+
+		action = rich.show_menu();
+		Console console = System.console();
+		double amount = 0;
+
+		switch (action) {
+			case 1: System.out.println(ac1.get_balance());
+				break;
+			case 2: amount = Double.parseDouble(console.readLine("Amount to deposit: "));
+				System.out.println("HIHI");
+				ac1.deposit(amount);
+				break;
+			case 3: amount = Double.parseDouble(console.readLine("Amount to withdraw: "));
+				System.out.println("HIHI");
+				ac1.withdraw(amount);
+				break;
+			case 4: 
+				break;
+			default: System.out.println("Not a valid input.");	
+				break;
+		}	
+		ac1.write();
+
+
+		amount = Double.parseDouble(console.readLine("Amount to deposit: "));
+		System.out.println(ac1.get_balance());
 	}
 }
 
